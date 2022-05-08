7c2a0b38fbedda316b16cbbce05e9bb738768cd0
Mokxrock <81343701+Mokxrock@users.noreply.github.com>
2022-05-08 19:37:44 +1000

    Update Task8.1.py

--- Task8.1.py
+++ Task8.1.py
@@ -1,19 +1,17 @@
 
-from smbus import SMBus
+from smbus import SMBus # Library use for I2C
 
-addr= 0x8 #buss Address
-bus = SMBus(1)
-
-numb = 1 
+addr= 0x8 #bus Address of the Slave
+bus = SMBus(1) # By using thos object it indicate that it is using the I2C-1 connection of the raspberry pi
 
 
 print("Enter 1 for LED ON or 0 for LED OFF")
 
-while 1:
-    ledstate = input(">>>>>>  ")
+while 1: # True
+    ledstate = input(">>>>>>  ") # Takes in User Input 
     if ledstate == "1":
-        bus.write_byte(addr, 0x1) # LED ON
+        bus.write_byte(addr, 0x1) # LED ON by telling the i2c bus
     elif ledstate == "0":
-        bus.write_byte(addr, 0x0) # LED OFF
+        bus.write_byte(addr, 0x0) # LED OFF by telling the i2c bus
     else:
-        break
+        break # if other value it will pause the loop 
